04c1a9e0be6cfb7839ed86af6de9dbe506cfb528
Erdal Erdem <erdalerdem@MacBook-Pro-tilhrende-Erdal.local>
2024-03-01 19:02:09 +0100

    Nav-bar fixing

--- 01._LearningLocker/app.js
+++ 01._LearningLocker/app.js
@@ -4,8 +4,23 @@ const app = express();
 const PORT = 8080;
 
 app.get("/", (req, res) => {
-    res.sendFile(__dirname + "/public/homepage.html")
+    res.sendFile(__dirname + "/public/pages/homepage.html")
 });
+
+app.get("/contact", (req, res) => {
+    res.sendFile(__dirname + "/public/pages/contact.html")
+});
+
+app.get("/aboutme", (req, res) => {
+    res.sendFile(__dirname + "/public/pages/aboutme.html")
+});
+
+app.get("/learninggoals", (req, res) => {
+    res.sendFile(__dirname + "/public/pages/learninggoals.html")
+});
+
+
+
 
 app.listen(PORT, () => {console.log("Server is running on port:", PORT)});
 
