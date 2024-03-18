cf73202f4bcf4e195a9a5973047fc501ab784237
Erdal Erdem <erdalerdem@MacBook-Pro-tilhrende-Erdal.local>
2024-03-18 15:47:51 +0100

    updating code

--- 01._LearningLocker/app.js
+++ 01._LearningLocker/app.js
@@ -14,28 +14,3 @@ app.use(pagesRouter);
 const PORT = 8080;
 app.listen(PORT, () => console.log("Server is running on port", PORT));
 
-
-// Old code
-
-/*
-
-app.get("/contact", (req, res) => {
-    res.sendFile(__dirname + "/public/pages/contact.html")
-});
-
-app.get("/aboutme", (req, res) => {
-    res.sendFile(__dirname + "/public/pages/aboutme.html")
-});
-
-app.get("/learninggoals", (req, res) => {
-    res.sendFile(__dirname + "/public/pages/learninggoals.html")
-});
-
-*/
-
-
-
-
-app.listen(PORT, () => {console.log("Server is running on port:", PORT)});
-
-module.exports = app;
